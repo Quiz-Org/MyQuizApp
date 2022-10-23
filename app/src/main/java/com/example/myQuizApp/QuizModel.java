d9c98d7d263a593bb9d831376af2b5992e7f4bcc
FegrusB <fergus@beckerleg.co.uk>
2022-10-24 00:04:31 +0100

    23/10/2022 Almost done with REST implementation.

--- app/src/main/java/com/example/myQuizApp/QuizModel.java
+++ app/src/main/java/com/example/myQuizApp/QuizModel.java
@@ -5,18 +5,16 @@ import com.google.gson.annotations.SerializedName;
 public class QuizModel {
 
     @SerializedName("_id")
-    public int id;
+    private int id;
 
     @SerializedName("Name")
-    public String name;
+    private String name;
 
     @SerializedName("Desc")
-    public String desc;
+    private String desc;
 
     public QuizModel(int id,String name, String desc){this.id = id;this.name=name;this.desc=desc;}
-    public QuizModel() {
-        ;
-    }
+    public QuizModel(){}
 
     public int get_id(){return id;}
     public String getName(){return name;}
